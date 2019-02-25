616aab08227973ec7ff690a089db3624cb06d027
Jasongrey <doctorsimple@gmail.com>
2019-02-25 15:34:30 -0600

    =Adding a block, twig template and hook_theme function

---
+++ src/Plugin/Block/ComparatorBlock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\comparator\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "comparator_block",
+ *   admin_label = @Translation("Comparator Block"),
+ * )
+ */
+class ComparatorBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#theme' => 'comparator',
+    );
+  }
+
+}
