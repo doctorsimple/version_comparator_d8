2534b5b5f49729d8673a68e9336f4d7fbe3d9763
Jasongrey <doctorsimple@gmail.com>
2019-02-27 15:05:50 -0600

    more setting dependencies, and changing the logic on the form autofill

--- js/form-autofill.js
+++ js/form-autofill.js
@@ -1,20 +1,11 @@
   Drupal.behaviors.formautofill = {
     attach: function (context, settings) {
       var $titlefield = jQuery("input[data-drupal-selector=edit-title-0-value]");
-      // jQuery('#node-text-for-comparison-form').on('submit', function(e){
-      //   var $titlefield = jQuery("input[data-drupal-selector=edit-title-0-value]");
-      //   if ($titlefield.val() == '') {
-      //   //  var book = jQuery("input[data-drupal-selector=edit-field-comparator-book-0-value]").val().split(' ', 2);
-      //  //   var author = jQuery("input[data-drupal-selector=edit-field-comparator-author-0-value]").val().split(' ').pop();
-      //     var chapter = jQuery("input[data-drupal-selector=edit-field-comparator-chapter-0-value]").val();
-      //     var thetitle = chapter + '.'
-      //     $titlefield.val(thetitle);
-      //   }
-      // });
+
       jQuery('#node-text-for-comparison-form').on('change',
           'input[data-drupal-selector="edit-field-comparator-chapter-0-value"]',
           function(e){
-            if ($titlefield.val() == '' ) {
+            if ( !$titlefield.val().match(/[a-zA-Z]/g) ) {
               $titlefield.val(e.target.value + '.');
             }
       });
